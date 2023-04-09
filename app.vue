5c8b5b56083230667a27c4a57964bba28b926d49
Wouter Vernaillen <wouter@vernaillen.com>
2023-04-09 11:15:04 +0200

    binnenkort

--- app.vue
+++ app.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="block text-center align-bottom min-h-screen">
-    <h1 class="text-4xl xl:text-5xl font-medium text-primary my-32 animate__animated animate__pulse">
+    <h4 class="text-xl sm:text-2xl mt-16 text-gray-400">
+      binnenkort:
+    </h4>
+    <h1 class="text-4xl xl:text-5xl font-medium text-primary my-20 animate__animated animate__pulse">
       Ecstatic Dance Zottegem
     </h1>
-    <h2 class="text-xl sm:text-2xl leading-relaxed text-gray-500 my-5 animate__animated animate__tada animate__delay-1s">
+    <h2 class="text-xl sm:text-2xl text-gray-500 mt-24 animate__animated animate__tada animate__delay-1s">
       zomer 2023
     </h2>
   </div>
