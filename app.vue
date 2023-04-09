401fdd6fbf5212396c56e0e864b41c5b960b2332
Wouter Vernaillen <wouter@vernaillen.com>
2023-04-09 10:46:44 +0200

    project setup, tailwind, animate.css & google fonts

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="block text-center align-bottom min-h-screen">
+    <h1 class="text-4xl xl:text-5xl font-medium text-primary my-32 animate__animated animate__pulse">
+      Ecstatic Dance Zottegem
+    </h1>
+    <h2 class="text-xl sm:text-2xl leading-relaxed text-gray-500 my-5 animate__animated animate__tada animate__delay-1s">
+      zomer 2023
+    </h2>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/tailwindcss',
+  ],
+  css: [
+    '@/assets/css/main.css',
+    'animate.css/animate.min.css',
+  ],
+  googleFonts: {
+    download: true,
+    preconnect: true,
+    display: 'swap',
+    families: {
+      Ubuntu: [400, 500, 700],
+    },
+  },
 })
