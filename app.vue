8fc6a106a337d43a3831e92945d049bd6d0de7ee
Wouter Vernaillen <wouter@vernaillen.com>
2023-05-20 14:17:19 +0200

    update

--- app.vue
+++ app.vue
@@ -1,12 +1,32 @@
+<script setup lang="ts">
+const title = 'Ecstatic Dance Zottegem'
+useHead({
+  title,
+  htmlAttrs: {
+    lang: 'en'
+  },
+  meta: [
+    {
+      name: 'description',
+      content: 'Healing power of Sound, Music & Dance'
+    },
+    {
+      property: 'og:title',
+      content: title
+    }
+  ]
+})
+</script>
+
 <template>
   <div class="block text-center align-bottom min-h-screen">
-    <h4 class="text-xl sm:text-2xl mt-16 text-gray-400">
+    <h4 class="text-xl sm:text-2xl mt-16 text-gray-600">
       binnenkort:
     </h4>
-    <h1 class="text-4xl xl:text-5xl font-medium text-primary my-20">
+    <h1 class="text-4xl xl:text-5xl font-bold text-primary my-20">
       Ecstatic Dance Zottegem
     </h1>
-    <h2 class="text-xl sm:text-2xl text-gray-500 mt-24">
+    <h2 class="text-xl sm:text-2xl text-gray-600 mt-24">
       zomer 2023
     </h2>
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,20 +3,21 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/google-fonts',
     '@nuxtjs/plausible',
-    '@nuxtjs/tailwindcss',
+    '@nuxtjs/tailwindcss'
   ],
   css: [
-    '@/assets/css/main.css',
+    '@/assets/css/main.css'
   ],
   googleFonts: {
     download: true,
+    inject: true,
     preconnect: true,
     display: 'swap',
     families: {
-      Ubuntu: [400, 500, 700],
-    },
+      Montserrat: [100, 200, 300, 400, 500, 600, 700, 800, 900]
+    }
   },
   plausible: {
-    apiHost: 'https://ecstaticdancezottegem.be/plio',
-  },
+    apiHost: 'https://ecstaticdancezottegem.be/plio'
+  }
 })
