4d458aa11b1ccfb232b298848ad059dd8d57bc69
Wouter Vernaillen <wouter@vernaillen.com>
2023-07-21 09:44:31 +0200

    preload mic32 font

--- app.vue
+++ app.vue
@@ -14,11 +14,35 @@ useHead({
       property: 'og:title',
       content: title
     }
+  ],
+  link: [
+    {
+      rel: 'preload',
+      as: 'font',
+      type: 'font/woff2',
+      crossorigin: '',
+      href: '/fonts/58405e358c3b387807fb206f187b0aa4-webfont.woff2'
+    },
+    {
+      rel: 'preload',
+      as: 'font',
+      type: 'font/woff2',
+      crossorigin: '',
+      href: '/fonts/montserrat-light-webfont.woff2'
+    },
+    {
+      rel: 'preload',
+      as: 'font',
+      type: 'font/woff2',
+      crossorigin: '',
+      href: '/fonts/montserrat-regular-webfont.woff2'
+    }
   ]
 })
 </script>
 
 <template>
+  <NuxtLoadingIndicator id="pageTop" color="repeating-linear-gradient(to right,rgb(197 213 45/100%) 0%,rgb(170 168 87/100%) 33%,rgb(157 177 159/100%) 66%,rgb(0 71 69/100%) 100%)" />
   <div class="prose block text-center align-bottom">
     <h4 class="text-xl sm:text-2xl mt-16 text-gray-600">
       binnenkort
